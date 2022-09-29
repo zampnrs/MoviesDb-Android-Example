42d48739180776505a2c69178c4065857be4ea53
Nicolas Zamprogno <znicolasrs@gmail.com>
2022-09-29 09:36:38 -0300

    Creating kotlin extensions file

---
+++ app/src/main/java/br/zampnrs/themoviesdbapi_example/utils/Extensions.kt
@@ -0,0 +1,12 @@
+package br.zampnrs.themoviesdbapi_example.utils
+
+import android.widget.Toast
+import androidx.fragment.app.Fragment
+
+
+fun Fragment.showToast(text: String, duration: Int = Toast.LENGTH_SHORT) {
+    Toast.makeText(activity?.applicationContext,
+        text,
+        duration
+    ).show()
+}
