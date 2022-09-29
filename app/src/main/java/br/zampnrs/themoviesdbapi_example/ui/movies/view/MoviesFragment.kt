345a9b7432b9d4ca6069f18be32646a48c9c14ea
Nicolas Zamprogno <znicolasrs@gmail.com>
2022-09-29 09:42:40 -0300

    Creating recycler view for movies

--- app/src/main/java/br/zampnrs/themoviesdbapi_example/ui/movies/view/MoviesFragment.kt
+++ app/src/main/java/br/zampnrs/themoviesdbapi_example/ui/movies/view/MoviesFragment.kt
@@ -3,6 +3,8 @@ package br.zampnrs.themoviesdbapi_example.ui.movies.view
 import android.os.Bundle
 import android.view.View
 
+import androidx.recyclerview.widget.LinearLayoutManager
+
 import br.zampnrs.themoviesdbapi_example.databinding.FragmentMoviesBinding
 import br.zampnrs.themoviesdbapi_example.utils.BaseFragment
 
@@ -11,6 +13,12 @@ class MoviesFragment : BaseFragment<FragmentMoviesBinding>(FragmentMoviesBinding
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        binding.setRecyclerView()
+    }
+
+    private fun FragmentMoviesBinding.setRecyclerView() {
+        moviesRecyclerView.layoutManager = LinearLayoutManager(context)
     }
 
 }
